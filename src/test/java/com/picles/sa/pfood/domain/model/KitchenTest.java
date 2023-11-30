aa77f39e0783eecb72b5060cd3c5d896c7eeb3f8
Perycles Junior <perycles@gmail.com>
2023-11-30 09:53:08 -0600

    fix(Test): Unit test Kitchen
    Made some fixes in tests of the Kitchen domain

--- src/test/java/com/picles/sa/pfood/domain/model/KitchenTest.java
+++ src/test/java/com/picles/sa/pfood/domain/model/KitchenTest.java
@@ -1,24 +1,46 @@
 package com.picles.sa.pfood.domain.model;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class KitchenTest {
 
-    private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    Kitchen kitchen;
-    @Test
-    public void veriifytLengthmorethan30(){
 
+    private Kitchen kitchen;
+    private Validator validator;
+
+    @BeforeEach
+    public void setup(){
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
         kitchen = new Kitchen();
         kitchen.setName("012345678901234567890123457890123123456");
-        assertThrows(ConstraintViolationException.class, () -> {
-            validator.validate( kitchen);
-        });
-
     }
+    @Test
+    public void veriifytLengthmorethan30(){
+        System.out.println(validator.validate( kitchen));
+        Set<ConstraintViolation<Kitchen>> violations = validator.validate( kitchen);
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    public void veriifytLengthLessthan30(){
+        kitchen.setName("less than 30");
+        System.out.println(validator.validate( kitchen));
+        Set<ConstraintViolation<Kitchen>> violations = validator.validate( kitchen);
+        assertTrue(violations.isEmpty());
+    }
+
 }
